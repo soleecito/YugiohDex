c36ac9702023ef1a7006dd49cce1d2add1b4bb34
soleecito <soledadrotiroti@gmail.com>
2021-09-29 21:55:42 -0300

    fragment cardList

---
+++ app/src/main/java/com/elvisoperator/yugiohdex/CardAdapter.kt
@@ -0,0 +1,26 @@
+package com.elvisoperator.yugiohdex
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class CardAdapter(private val character: MutableList<Data>) :
+    RecyclerView.Adapter<CardHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return CardHolder(layoutInflater.inflate(R.layout.item_cards, parent , false))
+
+    }
+
+    override fun onBindViewHolder(holder: CardHolder, position: Int) {
+     val item = character[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount(): Int {
+       return character.size
+    }
+
+
+}
---
+++ app/src/main/java/com/elvisoperator/yugiohdex/CardListFragment.kt
@@ -0,0 +1,27 @@
+package com.elvisoperator.yugiohdex
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.elvisoperator.yugiohdex.databinding.FragmentCardListBinding
+
+class CardListFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentCardListBinding>(inflater,
+            R.layout.fragment_card_list,container,false)
+        return binding.root
+
+    }
+
+}
+
+
+
+
